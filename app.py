8b41141dd45b905468828001f3401a371b473978
Memo <hyperhorus@gmail.com>
2022-01-13 21:18:53 -0600

    Added correct key

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from recursos.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres://tdtiotsbrwapba:3dacb2d534a83c56f31f4fc09d02e6038775e288e20ba322a772b5f16639e479@ec2-18-234-17-166.compute-1.amazonaws.com:5432/dcb4u9o8hoq9f1')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgressql://tdtiotsbrwapba:3dacb2d534a83c56f31f4fc09d02e6038775e288e20ba322a772b5f16639e479@ec2-18-234-17-166.compute-1.amazonaws.com:5432/dcb4u9o8hoq9f1')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app) #facilita la creación de resoruces
