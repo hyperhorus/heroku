f8365078736588cb1304c26472fea7142e2c5c1e
Memo <hyperhorus@gmail.com>
2022-01-14 11:06:49 -0600

    Correcting the error 2

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from recursos.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app) #facilita la creación de resoruces
